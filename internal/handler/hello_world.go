79a008423befcc408a8a602d8d6aadb5dcb89ff6
xuannpp <phuxuan.px2003@gmail.com>
2025-04-05 14:32:52 +0700

    test: new resp

--- internal/handler/hello_world.go
+++ internal/handler/hello_world.go
@@ -6,5 +6,5 @@ import (
 )
 
 func (h *handler) HelloWorld(ctx context.Context, req *hello.HelloRequest) (*hello.HelloResponse, error) {
-	return &hello.HelloResponse{Message: "Hello World - Xuân"}, nil
+	return &hello.HelloResponse{Message: "Xin chào tất cả mọi người, đây là đoạn text vô nghĩa."}, nil
 }
